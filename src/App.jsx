2595f1dff8965996fd0e23dfafe2dc1c7620c9ed
Ankit Shaw <130822839+shaw-ankit@users.noreply.github.com>
2024-02-24 17:11:17 +0530

    Star wars Planets

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './Index.css';
+import PlanetsDirectory from './PlanetsDirectory';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PlanetsDirectory/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PlanetsDirectory.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+
+const listOfColor = [
+  "red",
+  "green",
+  "orange",
+  "blue",
+  "pink",
+  "tomato",
+  "gray ",
+  "skyblue",
+  "violet",
+  "lightgreen",
+];
+
+const PlanetsDirectory = () => {
+  const [planets, setPlanets] = useState([]);
+  const [nextPlanets, setNextPlanets] = useState("");
+  const [previousPlanets, setPerviousPlanets] = useState("");
+  const [residentDetails, setResidentDetails] = useState([]);
+  const [selectedPlanet, setSelectedPlanet] = useState("");
+
+  const fetchDetails = (url) => fetch(url).then((res) => res.json());
+
+  const getAllResidents = (residents) => {
+    Promise.all(residents.map(fetchDetails))
+      .then(setResidentDetails)
+      .catch(console.error);
+  };
+  //Fetching the planets api
+  async function fetchPlanets(Url) {
+    try {
+      const data = await fetchDetails(Url);
+      setPlanets(data.results);
+      setNextPlanets(data.next);
+      setPerviousPlanets(data.previous);
+      console.log(data);
+    } catch (error) {
+      console.error("Planets data is not fetch", error);
+    }
+  }
+  useEffect(() => {
+    fetchPlanets("https://swapi.dev/api/planets/?format=json");
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Star Wars Planets</h1>
+        <div className="card-wrapper">
+          {planets.map((planet, index) => {
+            const color = listOfColor[index];
+            const eleStyle = { background: color };
+            return (
+              <div key={index} className="planet-card">
+                <div style={eleStyle} className="circle">
+                  <h3>{planet.name}</h3>
+                </div>
+                <div className="content">
+                  <h6>POPULATION: {planet.population}</h6>
+                  <h6>CLIMATE: {planet.climate}</h6>
+                  <h6>TERRAIN: {planet.terrain}</h6>
+                </div>
+                {planet.residents.length > 0 ? (
+                  <div className="button">
+                    <button
+                      style={eleStyle}
+                      onClick={() => {
+                        setSelectedPlanet(planet.name);
+                        getAllResidents(planet.residents);
+                      }}
+                    >
+                      View Residents
+                    </button>
+                  </div>
+                ) : (
+                  <div className="no-data">No residents found.</div>
+                )}
+                {planet.name === selectedPlanet &&
+                  residentDetails.length > 0 && (
+                    <div style={eleStyle} className="residents">
+                      {residentDetails.map((each) => (
+                        <div key={each.name} className="resident-data">
+                          <div className="box">
+                            <p>Name: {each.name}</p>
+                            <p>Height: {each.height} cm</p>
+                            <p>Mass: {each.mass} kg</p>
+                            <p>Gender: {each.gender}</p>
+                          </div>
+                          <hr />
+                        </div>
+                      ))}
+                    </div>
+                  )}
+              </div>
+            );
+          })}
+        </div>
+        <div className="pagination">
+          {previousPlanets && (
+            <button className="previous" onClick={() => fetchPlanets(previousPlanets)}>
+              Previous
+            </button>
+          )}
+          {nextPlanets && (
+            <button className="next" onClick={() => fetchPlanets(nextPlanets)}>Next</button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PlanetsDirectory;
